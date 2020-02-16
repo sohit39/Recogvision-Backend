d2f2ce5cd789be35347b25b6b3127e2a686e3628
snavjivan <sarthak.navjivan@gmail.com>
2020-02-16 03:16:52 -0800

    Add face matching

--- app.py
+++ app.py
@@ -4,6 +4,10 @@
 import os
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
+import face_recognition
+import base64
+from PIL import Image
+from io import BytesIO
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -73,6 +77,45 @@ def delete():
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
+@app.route('/match', methods=['POST'])
+def match():
+    try:
+        img_string = request.json['base64']
+        img_data = base64.b64decode(img_string)
+        filename = 'unknown.jpg'
+        with open(filename, 'wb') as f:
+            f.write(img_data)
+        unknown_image = face_recognition.load_image_file(filename)
+
+        known_images = []
+        for person in people_ref.stream():
+            img_data = base64.b64decode(person['base64'])
+            filename = person['name'] + '.jpg'
+            with open(filename, 'wb') as f:
+                f.write(img_data)
+            known_images.append(face_recognition.load_image_file(filename))
+
+        known_encodings = []
+        try:
+            for image in known_images:
+                known_encodings.append(face_recognition.face_encodings(image)[0])
+            unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        except IndexError:
+            print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+            quit()
+
+        results = face_recognition.compare_faces(known_encodings, unknown_encoding)
+        i = 0
+        for r in results:
+            if r == True:
+                return jsonify({"name": get_known_faces()[i][:-4]}), 200
+            i += 1
+        if True not in results:
+            return jsonify({"name": "Unknown"}), 200
+
+    except Exception as e:
+        return f"An Error Occurred: {e}"
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
